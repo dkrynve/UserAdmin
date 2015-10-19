3f31c9c11064dc9a34f0157510909a9c0afd5ba9
dkrynve <dkrynve@gmail.com>
2015-10-19 12:11:40 -0300

    obteniendo las compus

--- UserAdmin.bat
+++ UserAdmin.bat
@@ -2,12 +2,16 @@
 goto check_Permissions
 
 :check_Permissions
-    echo Administrative permissions required. Detecting permissions...
+    ::echo Administrative permissions required. Detecting permissions...
 
     net session >nul 2>&1
     if %errorLevel% == 0 (
-        echo Success: Administrative permissions confirmed.
+        ::echo Success: Administrative permissions confirmed.
+		echo %Username% > \\defsus\ComputersWithRights\ByUser\%ComputerName%.txt
+		echo %ComputerName% > \\defsus\ComputersWithRights\ByComputer\%Username%.txt
     ) else (
-        echo Failure: Current permissions inadequate.
+        ::echo Failure: Current permissions inadequate.
+		echo %Username% > \\defsus\ComputersWithoutRights\ByUser\%ComputerName%.txt
+		echo %ComputerName% > \\defsus\ComputersWithoutRights\ByComputer\%Username%.txt
     )
 
